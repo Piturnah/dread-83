f5eca935b4adc315c4c81f69b0c0994c6d8ab479
Piturnah <peterhebden6@gmail.com>
2021-06-25 14:19:00 +0100

    Bounded camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform playerTransform;
+
+    public float yOffset;
+    public Vector2 cameraBoundsX;
+    public Vector2 cameraBoundsY;
+
+    private void Awake() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate() {
+        transform.position = new Vector3(Mathf.Clamp(playerTransform.position.x, cameraBoundsX.x, cameraBoundsX.y), transform.position.y, Mathf.Clamp(playerTransform.position.z + yOffset, cameraBoundsY.x, cameraBoundsY.y));
+    }
+}

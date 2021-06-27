e0181b670933c3c798d7d0c913028b2ffe1b1d00
Piturnah <peterhebden6@gmail.com>
2021-06-27 21:12:22 +0100

    test

--- Assets/Scripts/DieAfterTime.cs
+++ Assets/Scripts/DieAfterTime.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class DieAfterTime : MonoBehaviour
 {
-    float timeLimit = 1;
+    float timeLimit = 5;
 
     // Update is called once per frame
     void Update()
     {
         if(timeLimit < 0)
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
         timeLimit -= Time.deltaTime;
     }

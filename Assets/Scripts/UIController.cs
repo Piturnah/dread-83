81f44be3e1e6f18d358ef27ed3b0d06ee71b55a5
Piturnah <peterhebden6@gmail.com>
2021-06-27 18:43:22 +0100

    Scene changes

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
     public static string dialogText;
-    public static bool finishedDialog;
+    public static bool finishedDialog = true;
+
+    public TextMeshProUGUI dialogBox;
+
+    public string[] dialogs;
+    int dialogIndex = 0;
+    float charDelay = .04f;
+
+    private void Start() {
+        CameraController.cameraState = 1; // state 1 is for intro scene overlooking the cringe
+
+
+    }
+
+    private void Update() {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) && UIController.finishedDialog) {
+            StartCoroutine(Typewriter(dialogs[dialogIndex], charDelay));
+        }
+    }
 
     IEnumerator Typewriter(string story, float btwCharDelay) {
+        dialogIndex += 1;
         finishedDialog = false;
-        dialogText = "";
+        dialogBox.text = "";
 
         foreach (char c in story) {
-            dialogText += c;
-            if (dialogText.Length == story.Length) {
+            dialogBox.text += c;
+            if (dialogBox.text.Length == story.Length) {
                 finishedDialog = true;
             }
             yield return new WaitForSeconds(btwCharDelay);
